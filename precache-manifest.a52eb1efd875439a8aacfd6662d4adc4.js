df73eba25803c0eb633f8b04b29c77079750cbe8
Alex Gaytsuk <gayrsuk@gmail.com>
2020-04-19 18:58:00 +0300

    Updates

---
+++ precache-manifest.a52eb1efd875439a8aacfd6662d4adc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773944b4b39e4f2cc7b67c04e212dfdd",
+    "url": "/social-network-header/index.html"
+  },
+  {
+    "revision": "07d6c18ba1ec35b5ab79",
+    "url": "/social-network-header/static/css/main.b0bb74ff.chunk.css"
+  },
+  {
+    "revision": "8f1bfcc67fa919e944a4",
+    "url": "/social-network-header/static/js/2.dc514ce6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network-header/static/js/2.dc514ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d6c18ba1ec35b5ab79",
+    "url": "/social-network-header/static/js/main.148cb2b6.chunk.js"
+  },
+  {
+    "revision": "5fe57f211267ecf045cd",
+    "url": "/social-network-header/static/js/runtime-main.d0e82f2a.js"
+  }
+]);
