28b42a235c93c33933a48798e2dc86d4e71caa67
Alexandr Gaytsuk <gayrsuk@gmail.com>
2020-04-19 21:47:37 +0300

    Add # to href

--- src/components/mainContent/rightBar/RightBar.jsx
+++ src/components/mainContent/rightBar/RightBar.jsx
@@ -29,7 +29,7 @@ const RightBar = () => {
                 <ul className={items}>
                     <li className={itemActive}>
                         <a className={ linkActive }
-                            href="/"
+                            href="/#"
                         >
                             <span className={ itemText }>
                                 News
@@ -38,7 +38,7 @@ const RightBar = () => {
                     </li>
                     <li className={item}>
                         <a className={ link }
-                            href="/"
+                            href="/#"
                         >
                             <span className={ itemText }>
                                 Photos
@@ -47,7 +47,7 @@ const RightBar = () => {
                     </li>
                     <li className={item}>
                         <a className={ link }
-                            href="/"
+                            href="/#"
                         >
                             <span className={ itemText }>
                                 Recommendations
@@ -56,7 +56,7 @@ const RightBar = () => {
                     </li>
                     <li className={item}>
                         <a className={ link }
-                            href="/"
+                            href="/#"
                         >
                             <span className={ itemText }>
                                 Search
@@ -65,7 +65,7 @@ const RightBar = () => {
                     </li>
                     <li className={item}>
                         <a className={ link }
-                           href="/"
+                           href="/#"
                         >
                             <span className={ itemText }>
                                 Updates
@@ -74,7 +74,7 @@ const RightBar = () => {
                     </li>
                     <li className={item}>
                         <a className={ link }
-                           href="/"
+                           href="/#"
                         >
                             <span className={ itemText }>
                                 Comments
